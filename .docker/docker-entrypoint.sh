4fd4b82f4f6c2bb4332da3ddb2770f40f0ffbe49
marcinkozakk <marcinkozakk@gmail.com>
2021-05-27 23:10:14 +0200

    docker

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -7,13 +7,6 @@ if [[ "${ENABLE_CRONTAB:-0}" = "1" ]]; then
   echo "* * * * * php /var/www/html/artisan schedule:run >> /dev/null 2>&1" >> /etc/crontabs/www-data
 fi
 
-# Enable Laravel queue workers
-if [[ "${ENABLE_WORKER:-0}" = "1" ]]; then
-  mv -f /etc/supervisor.d/worker.conf.default /etc/supervisor.d/worker.conf
-else
-  rm -f /etc/supervisor.d/worker.conf.default
-fi
-
 # Enable Laravel horizon
 if [[ "${ENABLE_HORIZON:-0}" = "1" ]]; then
   mv -f /etc/supervisor.d/horizon.conf.default /etc/supervisor.d/horizon.conf
